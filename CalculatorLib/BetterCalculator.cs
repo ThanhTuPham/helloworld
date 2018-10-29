a079045137410e48745d789b5d22a56aabc66aa5
Tu Pham <phamthanhtu261293@gmail.com>
2018-10-29 17:37:47 +0100

    initial commit

---
+++ CalculatorLib/BetterCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLib
+{
+    class BetterCalculator:Calculator
+    {
+        public double Mul(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ HelloWorldVS/Program.cs
@@ -0,0 +1,15 @@
+﻿using CalculatorLib;
+using System;
+
+namespace HelloWorldVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
